2f1a791b85ec9e8fe2b6623cb74a70c9bccacfeb
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-09 16:55:25 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import find_packages, setup
+import setup_helper
+
+version = '0.0.1'
+
+cmdclass = setup_helper.version_checker(version, 'wavestate')
+
+setup(
+    version          = version,
+    packages         = find_packages(exclude = ['docs']),
+    install_requires =[],
+    extras_require   ={},
+    tests_require=['pytest'],
+    test_suite   = 'pytest',
+    cmdclass         = cmdclass,
+    zip_safe         = True,
+)
+
