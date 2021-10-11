dcb8fefc4d6c5c04a3ccecc6e9b081d6a0af7a61
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-11 12:05:53 -0400

    Getting the setuptools_scm to work

--- src/wavestate/collection/_version.py
+++ src/wavestate/collection/_version.py
@@ -13,9 +13,10 @@ version = '.'.join(str(v) for v in version_info)
 __version__ = version
 
 # this section of code is removed in "release" branch versions
-__version__ + '.git-???'
+__version__ = __version__ + '.git-???'
 try:
     import setuptools_scm
+    print("SCM", setuptools_scm.get_version())
     __version__ = setuptools_scm.get_version(fallback_version=__version__)
 # FIXME: fallback_version is not available in the buster version
 # (3.2.0-1)
